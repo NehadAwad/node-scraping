e1faa5f06cde3639ca99f8dac244b1a94f876ba2
nehadawad <nehadakash622@gmail.com>
2023-02-04 10:55:03 +0600

    car router added

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const port = process.env.PORT || 3000;
 
 //internal imports
 const stateRouter = require('./router/stateRouter');
+const otomotoRouter = require('./router/otomotoRouter');
 
 //routing setup
 app.use('/states', stateRouter);
+app.use('/otomoto', otomotoRouter);
 
 app.listen(port, () => {
     console.log(`Server is runnnig on port ${port}`);
